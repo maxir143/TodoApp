2a82356910ba6c8fac8b2e76bbd4c68f90622eab
Your Name <you@example.com>
2022-08-17 15:09:45 -0500

    starting todo app

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput , View, SafeAreaView, Button, ScrollView, Text} from 'react-native';
 
 export default function App() {
+  const todos = [
+    {
+      title: 'hacer app nativa',
+      completed: false,
+      time: 600
+    }
+  ]
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.mainView}>
+      <View style={styles.inputView}>
+        <TextInput placeholder='todo' />
+        <Button title='+' style={styles.addButton} />
+      </View>
+      <View style={styles.todoList}>
+        {todos.map((todo, index) => 
+          (<Text key={index}>
+            {todo.title}
+            {todo.completed ? <Button title='❌' /> :  <Button title='✔' /> }
+            {todo.completed ? null :  <Button title='+ 1hora'/> }
+          </Text>)
+        )}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainView: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputView: {
+    flex:1,
+    flexDirection:'row',
+    margin:50,
+    justifyContent: 'space-around',
+    width: '90%'
+  },
+  todoList: {
+    flex: 3
+  },
+  addButton: {
+    width:10,
+    height:10 
+  }
 });
